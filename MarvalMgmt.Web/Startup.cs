3d590d625120a8511171b130c0da2feab7f24c73
Godswill David <davidgodswill@gamil.com>
2022-11-21 01:41:18 +0100

    code cleanup

--- MarvalMgmt.Web/Startup.cs
+++ MarvalMgmt.Web/Startup.cs
@@ -32,8 +32,7 @@ namespace MarvalMgmt.Web
             options.UseSqlServer(connectionString, o =>
             {
                 o.EnableRetryOnFailure();
-            }));
-            //services.AddDatabaseDeveloperPageExceptionFilter();
+            }))
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
